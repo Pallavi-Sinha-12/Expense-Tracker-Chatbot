d9d8597b73b9741d3c53c9e811ccb1773b81b6ed
Pallavi-Sinha-12 <pallavisinha95829@gmail.com>
2023-10-02 13:23:54 +0530

    Updated DatabaseConfig

--- database/DatabaseConfig.py
+++ database/DatabaseConfig.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 class DatabaseConfig(Enum):
 
-    SQL_DATABASE_SERVER_NAME = "expense-tracker-database-server.database.windows.net"
-    SQL_DATABASE_NAME = "expense-tracker-database"
-    SQL_DATABASE_USERNAME = "expense-user"
-    KEY_VAULT_URL = "https://keyvault2806.vault.azure.net/"
-    SQL_DATABASE_PASSWORD_SECRET_NAME = "expense-sql-database-password"
+    SQL_DATABASE_SERVER_NAME = None
+    SQL_DATABASE_NAME = None
+    SQL_DATABASE_USERNAME = None
+    KEY_VAULT_URL = None
+    SQL_DATABASE_PASSWORD_SECRET_NAME = None
